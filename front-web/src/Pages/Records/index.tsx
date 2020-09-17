70f5682d0e773029e3688cb4ebf5b04a4dcb6e52
Samuel Borges <sammy_rb@hotmail.com>
2020-09-17 20:44:57 -0300

    Criando navegação com rotas

---
+++ front-web/src/Pages/Records/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Records = () =>(
+    <h1> records</h1>
+);
+
+export default Records;
---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Header from './components/Header';
+import Records from './Pages/Records';
+
+
+const Routes = () =>(
+    <BrowserRouter>
+        <Header />
+        <Switch>
+            <Route path="/" exact>      <Home />            </Route>
+            <Route path="/records">     <Records />         </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
