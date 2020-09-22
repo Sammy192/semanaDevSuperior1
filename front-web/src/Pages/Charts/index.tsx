a27fc61ed6d0b6cd5ec9b74a82b5b2b148abf523
Samuel Borges <sammy_rb@hotmail.com>
2020-09-22 09:15:47 -0300

    Configurando front-webb

--- front-web/src/Pages/Charts/index.tsx
+++ front-web/src/Pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     labels: [],
     series: []
 }
-
+//const BASE_URL = 'http://localhost:8080';
 const BASE_URL = 'https://sds1-samuel.herokuapp.com';
 
 const Charts = () => {
