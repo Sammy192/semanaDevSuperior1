d52abbe7a9e80c164279a7314f9abff9ac3a4fd7
Samuel Borges <sammy_rb@hotmail.com>
2023-07-16 09:13:35 -0300

    front url

--- front-web/src/Pages/Charts/index.tsx
+++ front-web/src/Pages/Charts/index.tsx
@@ -22,10 +22,10 @@ const initialPieData = {
 }
 
 //const BASE_URL = 'https://sds1-samuel.herokuapp.com';
-//const BASE_URL = 'https://semanadevsuperior1-production.up.railway.app/';
+const BASE_URL = 'https://semanadevsuperior1-production.up.railway.app/';
 
 
-const BASE_URL = 'http://localhost:8080'; 
+//const BASE_URL = 'http://localhost:8080'; 
 
 
 const Charts = () => {
--- front-web/src/Pages/Records/index.tsx
+++ front-web/src/Pages/Records/index.tsx
@@ -8,9 +8,9 @@ import Filters from '../../components/Filters';
 
 
 //const BASE_URL = 'https://sds1-samuel.herokuapp.com';
-//const BASE_URL = 'https://semanadevsuperior1-production.up.railway.app/';
+const BASE_URL = 'https://semanadevsuperior1-production.up.railway.app/';
 
-const BASE_URL = 'http://localhost:8080'; 
+//const BASE_URL = 'http://localhost:8080'; 
 
 
 const Records = () => {
